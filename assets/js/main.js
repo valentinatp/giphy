41aab2634d27026278131cb2283c62861b2cc3f5
valentinatp <valentina.toledoporma@gmail.com>
2017-07-12 17:22:17 -0400

    comienzo a trabajar en JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-main.js
+$(document).ready(function(){
+	var dibujarGifs = function(data){
+		var gif = "";
+		var url = "";
+		data.forEach(function(element){
+			gif = element.images.downsized_large.url;
+			url = element.bitly_git_url;
+			$("#elementos").append(armarTemplate(gif, url));
+		});
+	}
+	var armarTemplate = function(gif.url){
+		var t = "<div class='elemento'><img src='" + gif + "'/<a href='" + url + "'>Ver más</a></div>"
+		return t;
+	}
+})
